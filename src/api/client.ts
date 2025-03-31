bb3aa2f3eb4c3a7e9669b39a0bc8d7ffe6857bf4
Yousef Amirghofran <yamirghofran@gmail.com>
2025-04-01 00:03:46 +0200

    General Commit

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance with default config
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
